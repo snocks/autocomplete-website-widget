9f51939dd662bc12359e45c891a63e5106b7777a
{m_mushtaha} <{muhamed.mushtaha@outlook.com}>
2017-02-22 11:52:37 +0200

    test

--- utils.js
+++ utils.js
@@ -11,29 +11,21 @@ request.onreadystatechange = function(response) {
     if (request.status === 200) {
       var text = request.responseText;
       var final = text.split('\n')
-      // Loop over the JSON array.
       final.forEach(function(item) {
-        // Create a new <option> element.
         var option = document.createElement('option');
-        // Set the value using the item in the JSON array.
         option.value = item;
-        // Add the <option> element to the <datalist>.
         dataList.appendChild(option);
       });
 
-      // Update the placeholder text.
       input.placeholder = "e.g. datalist";
     } else {
-      // An error occured :(
       input.placeholder = "Couldn't load datalist options :(";
     }
   }
 };
 
-// Update the placeholder text.
 input.placeholder = "Loading options...";
 var text = JSON
 
-// Set up and make the request.
 request.open('GET', 'https://raw.githubusercontent.com/gazaskygeeks/bootcamp/master/words.txt', true)
 request.send();
